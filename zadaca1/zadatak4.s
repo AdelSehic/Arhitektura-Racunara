493944e089bfc8fc177071085ba367cb583f6ce8
AdelSehic <adelsehic95@gmail.com>
2023-03-18 23:56:01 +0100

    Cetvrti uradjen

--- zadaca1/zadatak4.s
+++ zadaca1/zadatak4.s
@@ -1,24 +1,18 @@
 .section .data
 .globl niz
-    niz: .word 1,2,3,4,5
+    niz: .word 1,2,3,4,5,6
     kraj_niza: 
 .globl result
-    result: .word
+    result: .word 0
 
 .section .text
 .globl main
 main:
-    la $t0, niz # adresa niza
-    la $t1, kraj_niza # adresa kraja niza
-    sub $t1, $t1, 4
-
-#    sub $t3, $t1, $t0 # razlika adresa, predstavlja velicinu niza * word
-#    rsl $t3, $t3, 2 # velicina niza / 4
-
-#    andi $t4, $t4, 0 # counter varijabla za niz
-
-
+    la $t0, niz         # adresa niza
+    la $t1, kraj_niza   # adresa kraja niza
+    
     # trebamo provjeriti da li je niz sortiran
+    sub $t1, $t1, 4     # zbog nacina na koji radi petlja ovo je neophodno
     loop:
         lw $t2, 0($t0)
         lw $t3, 4($t0)
@@ -26,12 +20,35 @@ main:
         addi $t0, $t0, 4
     bne $t0, $t1, loop
 
-    j returnnula
+    la $t0, niz         # adresa niza
+    la $t1, kraj_niza   # adresa kraja niza
+
+    sub $t8, $t1, $t0   # razlika adresa, broj bita u nizu
+    srl $t9, $t8, 3     # "index" srednjeg elementa
+    andi $t4, $t8, 1    # provjeri je li broj neparan
+
+    bne $t4, $0, neparni
+        add $t7, $t0, $t8
+        lw $t9, 0($t7)
+        lw $t7, -4($t7)
+        add $t7, $t7, $t9
+        srl $t7, $t7, 1
+        la $t4, result
+        sw $t7, 0($t4)
+        j returnnula
+    neparni:
+        add $t7, $t8, $t0   # dodaj broj bytea na adresu
+        lw $t7, 0($t7)      # ucitaj podatke iz te adrese u nizu
+        la $t5, result      # ucitaj adresu rezultata
+        sw $t7, 0($t5)      # zapisi iz niza u memoriju
+        j returnnula
 
     nije_sortiran:
         andi $t9, $t9, 0
-        lui $t0, 0xDEAD
-        addi $t9, $0, 0x0000FA11
+        lui $t9, 0xDEAD
+        ori $t9, $t9, 0x0000FA11
+        la $t8, result
+        sw $t9, 0($t8)
 
     returnnula:
         addiu $v0, $0, 0
