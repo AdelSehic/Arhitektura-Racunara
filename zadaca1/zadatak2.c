a48075f04d4b727af1650328919af6e0df8b9f99
AdelSehic <adelsehic95@gmail.com>
2023-03-18 15:10:52 +0100

    drugi do pola uradjen, ne znam moduo

---
+++ zadaca1/zadatak2.c
@@ -0,0 +1,9 @@
+int number = 10;
+
+int main(){
+    printf("%d", number%1);
+    printf("%d", number*22);
+    printf("%d", number*7);
+    printf("%d", number%8);
+    printf("%d", number%32);
+}
--- zadaca1/zadatak2.s
+++ zadaca1/zadatak2.s
@@ -1,1 +1,38 @@
 .section .data
+.globl number
+number: .word 10
+.globl result
+result: .word 0
+
+.section .text
+.globl main
+main:
+
+    la $t0, number
+    lw $t0, 0($t0)
+
+    # check if even
+    andi $t1, $t1, 1
+    xori $t1, $t1, 1 
+
+    # pomnozi sa 22
+    sll $t2, $t0, 4
+    sll $t3, $t0, 2
+    add $t2, $t2, $t3
+    sll $t3, $t0, 1
+    add $t2, $t2, $t3
+
+    # pomnozi sa 7
+    sll $t3, $t0, 3
+    sub $t3, $t3, $t0
+
+    # modulo 8
+    addi $t4, $t0, -8
+
+    # modulo 32
+
+    la $t2, result
+    sw $t1, 0($t2)
+
+    addiu $v0, $0, 0
+    jr $ra
