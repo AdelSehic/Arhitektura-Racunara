d53a2bf8d4722f28fabcf48cf8e030c9e0aa182b
AdelSehic <adelsehic95@gmail.com>
2023-04-12 22:32:50 +0200

    Fino komentarisan zadatak

--- testovi/sum_even.s
+++ testovi/sum_even.s
@@ -1,18 +1,18 @@
 .section .text
 .globl sum_even
 sum_even:
-  andi $v0, $0, 0      # postavi povratni registar na nulu
-  addiu $t9, $0, 0     # counter for for loop
+  andi $v0, $0, 0       # postavi povratni registar na nulu za sumiranje
+  addiu $t9, $0, 0      # counter for for loop
 loop:
-  slt $t8, $t9, $a1    # je li counter manji od broja elemenata
-  beq $t8, $0, napolje # ako je rezultat poredjenja 0, izadji
-  lh $t1, 0($a0)       # ucitaj 16bita iz $a0
-  andi $t2, $t1, 1
-  bne $t2, $0, skip
-  addu $v0, $v0, $t1
+  slt $t8, $t9, $a1     # je li counter manji od broja elemenata
+  beq $t8, $0, napolje  # ako je rezultat poredjenja 0, izadji
+  lh $t1, 0($a0)        # ucitaj 16bita iz $a0
+  andi $t2, $t1, 1      # provjeri da li je LSB nula
+  bne $t2, $0, skip     # ako nije, skipaj dodavanje
+  addu $v0, $v0, $t1    # dodaj arr[i] na sumu
 skip:
-  addiu $a0, $a0, 2
-  addiu $t9, $t9, 1
-  j loop
+  addiu $a0, $a0, 2     # inkrementiraj adresu citanja
+  addiu $t9, $t9, 1     # inkrementuj brojac
+  j loop                # vrati se u petlju
 napolje:
-  jr $ra
+  jr $ra                
--- testovi/sum_even_main.s
+++ testovi/sum_even_main.s
@@ -8,16 +8,16 @@ format: .asciiz "Even_numbers:%d\n"
 .section .text
 .globl main
 main:
-  addiu $sp, $sp, -64
-  sw $ra, 60($sp)
-  la $a0, array
-  addiu $a1, $0, 6
-  jal sum_even
-  la $a0, format
-  addu $a1, $0, $v0
-  jal printf
-  lw $ra, 60($sp)
-  addiu $sp, $sp, 64
+  addiu $sp, $sp, -64 # zauzmi stacka
+  sw $ra, 60($sp)     # sacuvaj ra na stacku
+  la $a0, array       # ucitaj adresu niza na prvi argument
+  addiu $a1, $0, 6    # ucitaj velicinu na drugi argument
+  jal sum_even        # skoci u funkciju
+  la $a0, format      # ucitaj format string u prvi argument
+  addu $a1, $0, $v0   # ucitaj povratnu vrijednost prve funkcije u drugi arg
+  jal printf          # skoci u printf
+  lw $ra, 60($sp)     # obnovi ra registar na orginalnu vrijednost
+  addiu $sp, $sp, 64  # obnovi stack pointer na orginalnu vrijednost
 
   addiu $v0, $0, 0
   jr $ra
