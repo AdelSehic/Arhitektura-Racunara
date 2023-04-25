7afeca8d4f13fdc8268b89aa02723e172310e52d
AdelSehic <adelsehic95@gmail.com>
2023-04-25 08:31:35 +0200

    sedma sedmica

--- Vjezbe/vjezbe5/checkIfPalindromeMain.c
+++ Vjezbe/vjezbe5/checkIfPalindromeMain.c
@@ -1,13 +1,10 @@
 #include <string.h>
-#include <stdio.h>
-
-int isPalindrome(const char*, int);
 
 int main()
 {
-  const char* s = "addkdda";
+  const char* s = "Hello world";
 
-  if(isPalindrome(s, strlen(s))) printf("%s je palindrom\n", s);
+  if(isPalindrome(s, strlen(S))) printf("%s je palindrom\n", s);
   else printf("nije palindrom\n");
 
   return 0;
